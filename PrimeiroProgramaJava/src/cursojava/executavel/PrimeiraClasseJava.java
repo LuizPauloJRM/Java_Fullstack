b5e367ca1c546a6e6e60c59183a5f52b9c2b47cf
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2025-09-21 22:06:56 -0300

    Primeira classe java executavel

---
+++ PrimeiroProgramaJava/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,8 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	//Método main auto executavel em Java 
+	public static void main(String[] args) {
+		System.out.println("Olá mundo !!");
+	}
+}
