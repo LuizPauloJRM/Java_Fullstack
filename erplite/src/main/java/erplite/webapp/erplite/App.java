a8480e64da0a40c359a013308dafeaa0d1236634
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2025-09-29 14:22:57 -0300

    update erp lite

---
+++ erplite/src/main/java/erplite/webapp/erplite/App.java
@@ -0,0 +1,13 @@
+package erplite.erplite;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
