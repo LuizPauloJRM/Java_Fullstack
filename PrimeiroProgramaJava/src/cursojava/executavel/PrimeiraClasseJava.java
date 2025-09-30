5dbcfdf45b03a16e4e12bf2b7d0e526a5a0ed6b4
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2025-09-30 15:39:12 -0300

    update classes

--- PrimeiroProgramaJava/src/cursojava/executavel/PrimeiraClasseJava.java
+++ PrimeiroProgramaJava/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,13 +1,13 @@
 package cursojava.executavel;
 
 public class PrimeiraClasseJava {
-	//Método main auto executavel em Java 
+	// Método main auto executavel em Java
 	public static void main(String[] args) {
-		//Valor padrão é 0 
-		int numero;
+		// Valor padrão é 0
+		int numero1 = 12;
 		int numero2 = 0;
-		
+		System.out.println(numero1);
 		System.out.println(numero2);
-		
+
 	}
 }
