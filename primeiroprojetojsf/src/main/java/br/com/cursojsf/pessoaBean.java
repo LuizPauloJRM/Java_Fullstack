52bfc6a6c85bf820a15a4a9294c997c864f742a9
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2025-10-01 15:37:07 -0300

    Scoped anotações para comportamento da tela

--- primeiroprojetojsf/src/main/java/br/com/cursojsf/pessoaBean.java
+++ primeiroprojetojsf/src/main/java/br/com/cursojsf/pessoaBean.java
@@ -1,8 +1,16 @@
 package br.com.cursojsf;
 
 import javax.faces.bean.ManagedBean;
-
 /*Nomeado para pessoaBean ao chamar Managed*/
+/*Scoped*/
+/*
+ * @NoneScope-Cada vez que for chamado é instanciado 
+ * @ViewScoped-Enquanto tiver a tela aberta a instancia vai ser uma so
+ * @SessionScoped- Mantem instancia durante diversas requisições, mantem na seção segurando os dados
+ * @RequestScoped-(Padrão) , vida curta , termina quando a resposta e enviada ao cliente
+ * @ApplicationScoped-Distribui os dados iguais para todas seções 
+ */
+
 @ManagedBean(name = "pessoaBean")
 public class pessoaBean {
 	private String nome;
@@ -10,7 +18,7 @@ public class pessoaBean {
 	private String nomeCompleto;
 
 	public String mostrarNome() {
-		nomeCompleto = nome +" "+ sobrenome;
+		nomeCompleto = nome + " " + sobrenome;
 		return "";
 	}
 
