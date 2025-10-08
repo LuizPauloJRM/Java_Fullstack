59427709e4a825adacddafc20696041f86de6cca
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2025-10-08 15:31:16 -0300

    Metodo interceptadp TransacionalInterceptor

--- curso-erp-e-primefaces-essencial/src/main/java/com/minierp/jsf/util/EntityManagerProducer.java
+++ curso-erp-e-primefaces-essencial/src/main/java/com/minierp/jsf/util/EntityManagerProducer.java
@@ -1,14 +1,30 @@
 package com.minierp.jsf.util;
-
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+
+@ApplicationScoped
 public class EntityManagerProducer {
-	EntityManagerFactory factory;
+	private EntityManagerFactory factory;
 	
 	public EntityManagerProducer() {
 		this.factory = Persistence.createEntityManagerFactory("minierp");
 		
+	}
+	
+	/*Método produto de EntityManagers*/
+	@Produces
+	@RequestScoped
+	public EntityManager createEntityManager() {
+		return this.factory.createEntityManager();
+	}
+	public void closeEntityManager(@Disposes EntityManager manager) {
+		manager.close();
 	}
 	
 }
